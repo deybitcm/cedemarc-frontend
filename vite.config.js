4f359814e28386d944b738f971ccf7949c5f4e50
Deybit Condori <david000anthony@gmail.com>
2025-11-28 18:00:41 -0500

    fix: update comments for base path configuration in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// Usa variable de entorno para base path
+// En desarrollo: VITE_BASE_PATH='/'
+// En producción (GitHub Pages): VITE_BASE_PATH='/cedemarc-frontend/'
+// const base =
+//   typeof import.meta !== 'undefined' &&
+//   import.meta.env &&
+//   import.meta.env.VITE_BASE_PATH
+//     ? import.meta.env.VITE_BASE_PATH
+//     : '/cedemarc-frontend/'
+
 export default defineConfig({
+  base: '/cedemarc-frontend/',
   plugins: [react()],
   css: {
     postcss: './postcss.config.cjs',
   },
-  base: '/cedemarc-frontend/',
 })
