0b3d9f7b2a41e1868130df9c603becd22b7942a9
Deybit Condori <david000anthony@gmail.com>
2025-11-28 18:36:43 -0500

    fix: ensure base path is set correctly in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -12,6 +12,7 @@ import react from '@vitejs/plugin-react'
 //     : '/cedemarc-frontend/'
 
 export default defineConfig({
+  base: '/cedemarc-frontend',
   plugins: [react()],
   css: {
     postcss: './postcss.config.cjs',
