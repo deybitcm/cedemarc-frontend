85cfff600a95569f3162f1f47281898ba330f562
Deybit Condori <david000anthony@gmail.com>
2025-11-29 03:25:11 -0500

    fix: update base URL in vite.config.js to include 'www'

--- vite.config.js
+++ vite.config.js
@@ -12,7 +12,7 @@ import react from '@vitejs/plugin-react'
 //     : '/cedemarc-frontend/'
 
 export default defineConfig({
-  base: 'https://cedemarc.com/', // Ajusta esto según tu entorno
+  base: 'https://www.cedemarc.com/', // Ajusta esto según tu entorno
   plugins: [react()],
   css: {
     postcss: './postcss.config.cjs',
